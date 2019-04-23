b01053a19dc3e9b07c65aaf951665c298e9790da
brayvasq <brayvasq@gmail.com>
2019-04-23 12:24:32 -0500

    Add predict scene UI

--- frontend/src/components/Predict.js
+++ frontend/src/components/Predict.js
@@ -11,12 +11,39 @@ class Predict extends Component{
         this.state = {
             url_get_param: '',
             url_post_param: '',
-            respuesta: []
+            scene_data: `{
+                "materiales": [
+                    {
+                        "idCategoria": 0,
+                        "nombre": "huevos"
+                    },
+                    {
+                        "idCategoria": 1,
+                        "nombre": "arepas"
+                    },
+                    {
+                        "idCategoria": 2,
+                        "nombre": "matequilla"
+                    },
+                    {
+                        "idCategoria": 3,
+                        "nombre": "chocolate"
+                    },
+                    {
+                        "idCategoria": 4,
+                        "nombre": "pan"
+                    }
+                ]
+            }`,
+            respuesta: [],
+            respuesta_escena: []
         }
         this.getPetition = this.getPetition.bind(this);
         this.postPetition = this.postPetition.bind(this);
+        this.predictScene = this.predictScene.bind(this);
         this.handleChangeGet = this.handleChangeGet.bind(this);
         this.handleChangePost = this.handleChangePost.bind(this);
+        this.handleChangeScene = this.handleChangeScene.bind(this);
     }
 
     handleChangeGet(event) {
@@ -25,6 +52,10 @@ class Predict extends Component{
 
     handleChangePost(event) {
         this.setState({url_post_param: event.target.value});
+    }
+
+    handleChangeScene(event){
+        this.setState({scene_data: event.target.value});
     }
 
     async getPetition(){
@@ -55,6 +86,14 @@ class Predict extends Component{
         console.log(files)
         let value = files.base64.replace('data:image/jpeg;base64,','');
         this.setState({url_post_param: value});
+    }
+
+    async predictScene(){
+        let data = JSON.parse(this.state.scene_data);
+        console.log(data);
+        const resp = await axios.post('http://127.0.0.1:5000/analisisEscena',data);
+        this.setState({respuesta_escena: resp.data});
+        console.log(resp);
     }
 
     render(){
@@ -119,6 +158,42 @@ class Predict extends Component{
                     </div>
                             
                 </div>
+            
+            
+                <div className="box">
+                    <div className="columns">
+                        <div class="column separator">
+                            <h1 className="title is-1"> Scene Predictor </h1>
+                            <form>
+                                <div className="field">
+                                    <label className="label">Image Base64</label>
+                                    <div className="control">
+                                        {/*<input className="input" type="password" placeholder="Password" />*/}
+                                        <textarea class="textarea" placeholder="10 lines of textarea" rows="10" value={this.state.scene_data} onChange={this.handleChangeScene}></textarea>
+                                    </div>
+                                </div>
+
+                                <div className="buttons">
+                                    <span className="button is-info" onClick={this.predictScene}>POST</span>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="column">
+                        <form>
+                                <div className="field">
+                                    <label className="label">Image Base64</label>
+                                    <div className="control">
+                                        <ReactJson src={this.state.respuesta_escena}></ReactJson>
+                                    </div>
+                                </div>
+
+                            </form>
+                        
+                        </div>
+                    </div>
+                            
+                </div>
+            
             </div>
         )
     }
